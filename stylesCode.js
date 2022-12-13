3f18b90366b022f29b7502b9cdf99dc5a418cd14
Juan Carlos Ramirez <juancarlosramirezguerrero@gmail.com>
2022-12-13 12:48:45 +0100

    carrusel con swip

--- stylesCode.js
+++ stylesCode.js
@@ -1,5 +1,3 @@
-var imgHero = 'https://imgs.search.brave.com/GpRAuqJ8PZcKvurK99AFxw1rY0zOLcBrPUl_vIY17ZI/rs:fit:1038:398:1/g:ce/aHR0cDovL3d3dy5w/cm90b2NvbG8ub3Jn/L2V4dGZpbGVzL2kt/MTAzNDMtY0cuMTUx/NjAuMS5qcGcucGFn/ZXNwZWVkLmNlLlpI/S0t4eW9KVE4uanBn';
-var imgEslogan = "https://imgs.search.brave.com/S2_zzqvj7WXxUw8Oi2jkGdPi5tZUbS6HjYXa9NmYMb8/rs:fit:790:790:1/g:ce/aHR0cDovL3d3dy5j/dXR5cGFzdGUuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA0L21heGlfZW50/cmFkYS5qcGc";
 
 $(document).ready(() => {
     let vista = new Vista();
@@ -15,6 +13,8 @@ $(document).ready(() => {
       this.header = $("header")
       this.footer = $("footer")
       
+      this.imgHero = 'https://imgs.search.brave.com/GpRAuqJ8PZcKvurK99AFxw1rY0zOLcBrPUl_vIY17ZI/rs:fit:1038:398:1/g:ce/aHR0cDovL3d3dy5w/cm90b2NvbG8ub3Jn/L2V4dGZpbGVzL2kt/MTAzNDMtY0cuMTUx/NjAuMS5qcGcucGFn/ZXNwZWVkLmNlLlpI/S0t4eW9KVE4uanBn';
+      this.imgEslogan = "https://imgs.search.brave.com/S2_zzqvj7WXxUw8Oi2jkGdPi5tZUbS6HjYXa9NmYMb8/rs:fit:790:790:1/g:ce/aHR0cDovL3d3dy5j/dXR5cGFzdGUuY29t/L3dwLWNvbnRlbnQv/dXBsb2Fkcy8yMDE2/LzA0L21heGlfZW50/cmFkYS5qcGc";
       this.carr1 = './images/bolso.webp'
       this.carr2 = './images/chicas.webp'
       this.carr3 = './images/collar.webp'
@@ -44,11 +44,11 @@ $(document).ready(() => {
       this.main.append(`<section id="siguenos"></section>`);
       this.main.append(`<section id="contacto"></section>`);
 
-      $("#hero").append(`<img src="${imgHero}" alt="hero">`);
+      $("#hero").append(`<img src="${this.imgHero}" alt="hero">`);
 
       $("#eslogan").append(`<div class='imgEslogan'></div>`);
       $("#eslogan").append(`<div class='textoEslogan'></div>`);
-      $(".imgEslogan").append(`<img src="${imgEslogan}" alt="Gente comprando"></img>`);
+      $(".imgEslogan").append(`<img src="${this.imgEslogan}" alt="Gente comprando"></img>`);
       $(".textoEslogan").append(`<h1>¡Bienvenido a tu tienda favorita!</h1>`);
       $(".textoEslogan").append(`<p>Encontrá todo lo que necesitás para tu hogar</p>`);
       $(".textoEslogan").append(`<p>¡Y mucho más!</p>`);
@@ -56,8 +56,14 @@ $(document).ready(() => {
 
       $("#carrusel").append(`<div id='imagenesCarrusel'></div>`);
       $("#imagenesCarrusel").append(`<img src="${this.carr1}"></img>`);
+      $("#carrusel").append(`<i class="fa-solid fa-arrow-right"></i>`);
+      $("#carrusel").append(`<i class="fa-solid fa-arrow-left"></i>`);
       this.hammertimeCarrusel = new Hammer(this.main.children("#carrusel").children("#imagenesCarrusel").children("img")[0]);
       this.rotarCarrusel(this.carruselfotos);
+
+      $("#siguenos").append(`<div class='imageneRedes'></div>`);
+      $("#siguenos").append(`<div class='textoRedes'></div>`);
+      $('#.imageneRedes').append(`<img src="${this.imgRedes}" alt="Redes sociales"></img>`);
     }
 
     rotarCarrusel(carrusel){
